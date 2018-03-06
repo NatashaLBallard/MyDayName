388b8024e867cc525820a1352d40a6854b2e0ed6
natashalballard <nbtcareer@gmail.com>
2018-03-06 11:35:37 -0500

    config git

--- src/main/java/com/mydayname/demo/MainController.java
+++ src/main/java/com/mydayname/demo/MainController.java
@@ -7,7 +7,6 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 import java.time.LocalDate;
